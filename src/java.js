ae82f5ec6b097cadaadb96d029525893bf1202d0
Mdnkosi <mahlatsedineo93@gmail.com>
2025-03-09 16:48:46 +0200

    API integration.

--- src/java.js
+++ src/java.js
@@ -1,14 +1,33 @@
+function showDestination(response) {
+  console.log(response.data.answer)
+   new Typewriter("#user-output", {
+     strings: response.data.answer,
+     autoStart: true,
+     delay: 30,
+     cursor: null,
+   });
+}
+
+
+
 function generateDestination(event) {
   event.preventDefault();
 
+let userInput = document.querySelector("#user-input")
+let apiKey = "t0dbadd020obaf40a394084940d3fadf";
+let context =
+  "You are a well informed travel assistant that knows what people want when they travel, from weather conditions to the best time to travel.Include the best time to travel to the destination.Paraphrase the answer and make it short-versioned";
+let prompt = `Provide the best place in the enitre world to travel to according to the weather selected in ${userInput.value}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+
   let destination = document.querySelector("#user-output");
-  destination.innerHTML= "heloo"
-  new Typewriter("#user-output", {
-    strings: ( "The best place to travel is..."),
-    autoStart: true,
-    delay: 30,
-    cursor:null,
-  });
+  destination.classList.remove("hidden");
+  destination.innerHTML = `<div class="traveling">🧳😎🌍lets travel ....</div>`;
+ 
+  axios.get(apiUrl).then(showDestination);
+
 }
 
 let travelDestination = document.querySelector("#container");
